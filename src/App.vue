4e6923ab8f52921c76eccecaf86484199fa2b5d9
Yurii <juriuys@gmail.com>
2021-09-08 01:13:19 +0300

    test
    change text

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="GitHub Actions are awesome!"/>
+    <HelloWorld msg="GitHub Actions are awesome!!!"/>
   </div>
 </template>
 
